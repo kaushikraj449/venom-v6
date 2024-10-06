e2a784da44a951c534e54b61fd170239a7bff843
kaushikraj449 <kaushikraj449@gmail.com>
2024-10-06 20:09:19 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/YOURREPONAME/sasuke.py",
+    "postStartCommand": "python3 /workspaces/YOURREPONAME/venom.py",
     "customizations": {
         "vscode": {
             "settings": {
